4e8730521455c5ba44cad0dbcc0d7aeac952151c
Mareenbck <marine.bascunana@gmail.com>
2024-02-03 18:14:01 +0100

    add prisma service and clean app module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe( {
+		whitelist: true,
+		}
+	));
+	app.enableCors({
+	});
+	app.get(PrismaService);
   await app.listen(3000);
 }
 bootstrap();
